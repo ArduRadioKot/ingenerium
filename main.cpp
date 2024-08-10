80cdd2513ec32f4800ef40702d60fff936dd27ae
ArduRadioKot <124770304+ArduRadioKot@users.noreply.github.com>
2024-08-10 16:59:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+void drawTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    drawTriangle(n);
+    return 0;
+}
